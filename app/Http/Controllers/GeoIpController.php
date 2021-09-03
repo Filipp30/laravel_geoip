f54f605e38095524454b52b9ed821a528521179e
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-09-03 18:33:39 +0200

    refactor run methode

--- app/Http/Controllers/GeoIpController.php
+++ app/Http/Controllers/GeoIpController.php
@@ -14,7 +14,12 @@ class GeoIpController extends Controller{
         logs()->info('Call run GeoIp...');
         return response([
             "message"=>'geo_ip_call',
-            "request"=>gettype($request)
+            "info1"=>gettype($request),
+            "info2"=>json_encode($request->query),
+            "info3"=>json_encode($request->query('data')),
+            "info4"=>json_encode($request->get('data')),
+            "info5"=>json_encode($request->get()),
+            "info6"=>$request['data']
         ]);
     }
 
