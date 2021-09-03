f1b2a1efe27d3f6e03d13bf90aab2e2f26a81441
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-09-03 21:12:00 +0200

    refactor run methode

--- app/Http/Controllers/GeoIpController.php
+++ app/Http/Controllers/GeoIpController.php
@@ -7,20 +7,13 @@ use Illuminate\Http\Request;
 class GeoIpController extends Controller{
 
     public function run(Request $request){
-        logs()->info(json_encode($request->request));
-        logs()->info(json_encode($request->query));
-        logs()->info(json_encode($request->query('data')));
-        logs()->info(json_encode($request->get('data')));
+        logs()->info($request->get('data'));
+        logs()->info($request['data']);
         logs()->info('Call run GeoIp...');
         return response([
             "message"=>'geo_ip_call',
-            "info1"=>gettype($request),
-            "info2"=>json_encode($request->query),
-            "info3"=>json_encode($request->query('data')),
-            "info4"=>$request->get('data'),
-            "info7"=>$request->query('data'),
-            "info6"=>$request['data'],
-            "info8"=>$request->get('data')
+            "data_1"=>$request->get('data'),
+            "data_2"=>$request['data'],
         ]);
     }
 
