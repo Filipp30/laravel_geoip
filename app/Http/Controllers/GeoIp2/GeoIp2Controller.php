8eb9c3b4f41941cba9bca1dae52b4fc135bf7a5f
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-09-05 15:31:05 +0200

    create get_location methode

--- app/Http/Controllers/GeoIp2/GeoIp2Controller.php
+++ app/Http/Controllers/GeoIp2/GeoIp2Controller.php
@@ -20,19 +20,27 @@ class GeoIp2Controller extends Controller
     {
         $client = new Client(602468, 'iaRImnahWwvDxuYn', ['en'], ['host' => 'geolite.info']);
         $country = $client->country($ip);
+        $city = $client->city($ip);
         return [
             'continent'=>[
-                'id'=>$country->continent->geonameId,
+                'geo_id'=>$country->continent->geonameId,
                 'iso_code'=>$country->continent->code,
                 'name'=>$country->continent->names['en']
             ],
             'country'=>[
-                'id'=>$country->country->geonameId,
+                'geo_id'=>$country->country->geonameId,
                 'iso_code'=>$country->country->isoCode,
                 'is_in_european_union'=>$country->country->isInEuropeanUnion,
                 'name'=>$country->country->names['en']
             ],
-            'traits'=>$country->traits
+            'city'=>[
+                'geo_id'=>$city->city->geonameId,
+                'name'=>$city->city->names['en'],
+                'postal_code'=>$city->postal->code,
+                'province'=>$city->subdivisions[0]->names['en'],
+                'location'=>$city->location,
+            ],
+            'internet_provider'=>$city->traits,
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use App\Http\Controllers\GeoIp2\GeoIp2Controller;
 use Illuminate\Support\Facades\Route;
 
