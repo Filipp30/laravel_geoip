f9a653aab43fa2f490f3eb28df8ddcaa38fd5b48
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-09-12 12:31:14 +0200

    create shl command to clean log files

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::get('/ip/run/{ip}',function ($ip){
 
     Route::get('/run/task/clean/log',function (){
 //        $process = new Process(['sudo /home/exdir/run.sh']);
-        $process = new Process(['cd', '/home/exdir','./run.sh']);
+        $process = new Process(['sudo','/home/exdir/run.sh']);
         $process->run();
 
         if (!$process->isSuccessful()) {
