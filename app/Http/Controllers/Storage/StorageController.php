156bac914f0af8d3ebdafaba394cc93500e5bb32
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-09-14 15:29:32 +0200

    refactor save,exists.download.delete methods+routes.Clean code.

--- app/Http/Controllers/Storage/StorageController.php
+++ app/Http/Controllers/Storage/StorageController.php
@@ -10,7 +10,23 @@ class StorageController extends Controller{
 
     public function put(Request $request){
         $file_name = Storage::disk('sftp')->put('files',$request->file('file'));
+
         return response([
+            'file_name'=>$file_name
+        ],201);
+    }
+
+    public function delete($dir,$file_name){
+        $file_path = $dir.'/'.$file_name;
+        $res = Storage::disk('sftp')->delete($file_path);
+        $exists = Storage::disk('sftp')->exists($file_path);
+        $missing = Storage::disk('sftp')->missing($file_path);
+
+        return response([
+            'delete'=>$res,
+            'exists'=>$exists,
+            'missing'=>$missing,
+            'dir_name'=>$dir,
             'file_name'=>$file_name
         ],201);
     }
@@ -18,6 +34,7 @@ class StorageController extends Controller{
     public function download($dir,$file_name){
         $file_path = $dir.'/'.$file_name;
         $exists = Storage::disk('sftp')->exists($file_path);
+
         if ($exists){
             return Storage::disk('sftp')->download($file_path);
         }else{
@@ -35,20 +52,6 @@ class StorageController extends Controller{
         $missing = Storage::disk('sftp')->missing($file_path);
 
         return response([
-            'exists'=>$exists,
-            'missing'=>$missing,
-            'dir_name'=>$dir,
-            'file_name'=>$file_name
-        ],201);
-    }
-
-    public function delete($dir,$file_name){
-        $file_path = $dir.'/'.$file_name;
-        $res = Storage::disk('sftp')->delete($file_path);
-        $exists = Storage::disk('sftp')->exists($file_path);
-        $missing = Storage::disk('sftp')->missing($file_path);
-        return response([
-            'delete'=>$res,
             'exists'=>$exists,
             'missing'=>$missing,
             'dir_name'=>$dir,
--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,15 @@ use App\Http\Controllers\VpsServer\ServerTaskController;
 use App\Jobs\GetGeoIp2DataJop;
 use Illuminate\Support\Facades\Route;
 
-//This request will be redirected by proxy server from port 80 to localhost port 8000
+//NGINX:This request will be redirected by proxy server from port 80 to localhost port 8000
 Route::get('/ip/run/{ip}',function ($ip){
         GetGeoIp2DataJop::dispatch($ip);
         return response(["ip"=>$ip],201);
 });
 
-Route::get('/run/task/clean/log',[ServerTaskController::class,'rm_log_files']);
 
 
-
-//SFTP Storage Routes
+//SFTP: Storage Routes
 Route::post('/save/file',[StorageController::class,'put']);
 
 Route::get('/exists/{dir}/{file_name}',function($dir,$file_name,StorageController $storage){
@@ -28,3 +26,7 @@ Route::get('/download/{dir}/{file_name}',function ($dir,$file_name,StorageContro
 Route::delete('/delete/{dir}/{file_name}',function ($dir,$file_name,StorageController $storage){
     return $storage->delete($dir,$file_name);
 });
+
+
+//VPS-server: Run terminal tasks from bash script
+Route::get('/run/task/clean/log',[ServerTaskController::class,'rm_log_files']);
