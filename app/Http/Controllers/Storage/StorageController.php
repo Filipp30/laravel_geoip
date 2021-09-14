9973d25358c6333a09ac33d44963785ac323b3d3
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-09-14 15:25:09 +0200

    Create save,exists.download.delete methods+routes

--- app/Http/Controllers/Storage/StorageController.php
+++ app/Http/Controllers/Storage/StorageController.php
@@ -9,11 +9,51 @@ use Illuminate\Support\Facades\Storage;
 class StorageController extends Controller{
 
     public function put(Request $request){
-        $status = Storage::disk('sftp')->put('files',$request->file('file'));
+        $file_name = Storage::disk('sftp')->put('files',$request->file('file'));
         return response([
-            'status'=>$status
+            'file_name'=>$file_name
         ],201);
     }
 
+    public function download($dir,$file_name){
+        $file_path = $dir.'/'.$file_name;
+        $exists = Storage::disk('sftp')->exists($file_path);
+        if ($exists){
+            return Storage::disk('sftp')->download($file_path);
+        }else{
+            return response([
+                'exists'=>$exists,
+                'dir_name'=>$dir,
+                'file_name'=>$file_name
+            ],201);
+        }
+    }
+
+    public function exists($dir,$file_name){
+        $file_path = $dir.'/'.$file_name;
+        $exists = Storage::disk('sftp')->exists($file_path);
+        $missing = Storage::disk('sftp')->missing($file_path);
+
+        return response([
+            'exists'=>$exists,
+            'missing'=>$missing,
+            'dir_name'=>$dir,
+            'file_name'=>$file_name
+        ],201);
+    }
+
+    public function delete($dir,$file_name){
+        $file_path = $dir.'/'.$file_name;
+        $res = Storage::disk('sftp')->delete($file_path);
+        $exists = Storage::disk('sftp')->exists($file_path);
+        $missing = Storage::disk('sftp')->missing($file_path);
+        return response([
+            'delete'=>$res,
+            'exists'=>$exists,
+            'missing'=>$missing,
+            'dir_name'=>$dir,
+            'file_name'=>$file_name
+        ],201);
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,23 @@ use Illuminate\Support\Facades\Route;
 
 //This request will be redirected by proxy server from port 80 to localhost port 8000
 Route::get('/ip/run/{ip}',function ($ip){
-
         GetGeoIp2DataJop::dispatch($ip);
-            return response([
-            "geo_ip2"=>"job called",
-            "ip"=>$ip
-        ],201);
-    });
+        return response(["ip"=>$ip],201);
+});
 
 Route::get('/run/task/clean/log',[ServerTaskController::class,'rm_log_files']);
 
+
+
+//SFTP Storage Routes
 Route::post('/save/file',[StorageController::class,'put']);
+
+Route::get('/exists/{dir}/{file_name}',function($dir,$file_name,StorageController $storage){
+   return $storage->exists($dir,$file_name);
+});
+Route::get('/download/{dir}/{file_name}',function ($dir,$file_name,StorageController $storage){
+    return $storage->download($dir,$file_name);
+});
+Route::delete('/delete/{dir}/{file_name}',function ($dir,$file_name,StorageController $storage){
+    return $storage->delete($dir,$file_name);
+});
