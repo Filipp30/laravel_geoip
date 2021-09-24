13a0fa966d531a9d8098c1df664ba3d8dcc98c31
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-09-24 13:43:08 +0200

    create HandleIpJob methode

---
+++ app/Jobs/HandleIpJob.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Http\Controllers\GeoIp2\GeoIp2Controller;
+use App\Models\AdminUsers;
+use App\Notifications\GeoIp2Notifications\GeoIpJobFailedNotification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Throwable;
+
+class HandleIpJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $failOnTimeout = true;
+
+    public $tries = 1;
+
+    public $maxExceptions = 1;
+
+    public $timeout = 15;
+
+    protected $ip;
+
+    public function __construct($ip){
+        $this->ip = $ip;
+    }
+
+    public function handle(GeoIp2Controller $geoIp2Controller){
+        $geoIp2Controller->handle($this->ip);
+    }
+
+    public function failed(Throwable $exception){
+        $user = AdminUsers::query()->findOrFail(1);
+        $user->notify(new GeoIpJobFailedNotification($exception));
+    }
+}
