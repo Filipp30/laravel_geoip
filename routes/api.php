a38691d7cf77d9ec21521872463f4fb8b722b01f
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-09-25 15:56:18 +0200

    Create Email Verification routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\RegistrationController;
 use App\Http\Controllers\Storage\StorageController;
 use App\Http\Controllers\VpsServer\ServerTaskController;
@@ -8,32 +9,40 @@ use Illuminate\Support\Facades\Route;
 
 
 //Nginx: Save Ip address when visiting th web-site.
-Route::get('/ip/run/{ip}',function ($ip){
-    HandleIpJob::dispatch($ip);
-    return response(['ip'=>$ip],201);
-});
+    Route::get('/ip/run/{ip}',function ($ip){
+        HandleIpJob::dispatch($ip);
+        return response(['ip'=>$ip],201);
+    });
 
 
 //SFTP: Storage Routes # SFTP server created on the VPS server.
-Route::post('/save/file',[StorageController::class,'put']);
+    Route::post('/save/file',[StorageController::class,'put']);
 
-Route::get('/exists/{dir}/{file_name}',function($dir,$file_name,StorageController $storage){
-   return $storage->exists($dir,$file_name);
-});
-Route::get('/download/{dir}/{file_name}',function ($dir,$file_name,StorageController $storage){
-    return $storage->download($dir,$file_name);
-});
-Route::delete('/delete/{dir}/{file_name}',function ($dir,$file_name,StorageController $storage){
-    return $storage->delete($dir,$file_name);
-});
+    Route::get('/exists/{dir}/{file_name}',function($dir,$file_name,StorageController $storage){
+       return $storage->exists($dir,$file_name);
+    });
+    Route::get('/download/{dir}/{file_name}',function ($dir,$file_name,StorageController $storage){
+        return $storage->download($dir,$file_name);
+    });
+    Route::delete('/delete/{dir}/{file_name}',function ($dir,$file_name,StorageController $storage){
+        return $storage->delete($dir,$file_name);
+    });
 
 
 //VPS-server: Run bash script #home/exedir/run.sh  on the server.
-Route::get('/run/task/rm/log',[ServerTaskController::class,'rm_log']);
-Route::get('/run/task/ping/{ip}',function ($ip,ServerTaskController $serverTaskController){
-    return $serverTaskController->ping_ip($ip);
-});
+    Route::get('/run/task/rm/log',[ServerTaskController::class,'rm_log']);
+    Route::get('/run/task/ping/{ip}',function ($ip,ServerTaskController $serverTaskController){
+        return $serverTaskController->ping_ip($ip);
+    });
 
 
 //AUTH: Authentication and Authorization routes.
-Route::post('/auth/registration',[RegistrationController::class,'registration']);
+    Route::post('/auth/registration',[RegistrationController::class,'registration']);
+
+    ### Email Verification ###
+    Route::get('/email/verify/{id}/{hash}',[EmailVerificationController::class,'emailVerify'])
+    ->middleware(['auth.email.verification'])->name('verification.verify');
+
+    Route::get('/email/verification-notification/{id}',[EmailVerificationController::class,'sendEmailVerificationNotification'])
+    ->name('verification.send');
+
