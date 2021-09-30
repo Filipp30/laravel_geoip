7ac84698f93c6b4f6badba691ed1898daeda1318
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-09-30 15:06:14 +0200

    refactor

--- app/Repository/CreateDatabase/SqlManager.php
+++ app/Repository/CreateDatabase/SqlManager.php
@@ -10,14 +10,14 @@ class SqlManager implements DatabaseRepositoryInterface {
 
     public function databaseExists($database_name): bool
     {
-        $process = new Process(['/home/exedir/mysql_tasks/db_exists.sh '.$database_name]);
+        $process = new Process(['/home/exedir/mysql_tasks/db_exists.sh'.' '.$database_name]);
         $process->run();
         return boolval($process->getOutput());
     }
 
     public function userExists($user_name): bool
     {
-        $process = new Process(['/home/exedir/mysql_tasks/user_exists.sh '.$user_name]);
+        $process = new Process(['/home/exedir/mysql_tasks/user_exists.sh'.' '.$user_name]);
         $process->run();
         return boolval($process->getOutput());
     }
