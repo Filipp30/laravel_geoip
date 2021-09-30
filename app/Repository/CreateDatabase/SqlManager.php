a707c713ed8172dbc5a670bb893ed3f5721dddbb
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-09-30 19:04:42 +0200

    fix bug in bash script: testing

--- app/Repository/CreateDatabase/SqlManager.php
+++ app/Repository/CreateDatabase/SqlManager.php
@@ -8,7 +8,7 @@ use Symfony\Component\Process\Process;
 
 class SqlManager implements DatabaseRepositoryInterface {
 
-    public function databaseExists($database_name)
+    public function databaseExists($database_name): bool
     {
         $process = new Process(['/home/exedir/mysql_tasks/db_exists.sh',$database_name]);
         $process->run();
@@ -16,10 +16,10 @@ class SqlManager implements DatabaseRepositoryInterface {
         $isSuccessful = $process->isSuccessful();
         $error = $process->getErrorOutput();
 
-        return $process->getOutput();
+        return boolval($process->getOutput());
     }
 
-    public function userExists($user_name)
+    public function userExists($user_name): bool
     {
         $process = new Process(['/home/exedir/mysql_tasks/user_exists.sh',$user_name]);
         $process->run();
@@ -27,7 +27,7 @@ class SqlManager implements DatabaseRepositoryInterface {
         $isSuccessful = $process->isSuccessful();
         $error = $process->getErrorOutput();
 
-        return $process->getOutput();
+        return boolval($process->getOutput());
     }
 
     public function create()
